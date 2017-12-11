0edc09d379edf90d8d15f02785042014b4f8e032
AMWJ <arielj@mit.edu>
2017-12-11 14:52:16 -0500

    Using Key matching for coercion

--- src/java/simpledb/json/JsonScan.java
+++ src/java/simpledb/json/JsonScan.java
@@ -16,8 +16,10 @@ import simpledb.Tuple;
 import simpledb.TupleDesc;
 import simpledb.Type;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 import java.util.Queue;
 
@@ -28,6 +30,7 @@ public class JsonScan implements OpIterator {
     private int tableId;
     private int index = 0;
     private Queue<Tuple> tupBuffer = new LinkedList<>();
+    static private TupleDesc tableToInsertIntoDesc = new TupleDesc(new Type[] {Type.INT_TYPE},new String[] {"_tableid"});
 
     public JsonScan(TransactionId t, JsonElement je, int table) {
         this.tid = t;
@@ -95,54 +98,131 @@ public class JsonScan implements OpIterator {
         TupleDesc td = catalog.getTupleDesc(table);
         Map<Integer, Integer> refs = catalog.getForeignKeys(table);
 
-        Tuple tup = new Tuple(td);
+        Tuple tup = new Tuple(TupleDesc.merge(td, tableToInsertIntoDesc));
+        
+        KeyMatching matching = new KeyMatching(obj);
+        
         for (int i = 0; i < td.numFields(); i++) {
             String fieldName = td.getFieldName(i);
             Type fieldType = td.getFieldType(i);
 
-            JsonElement rawVal = obj.get(fieldName);
-            if (rawVal == null) {
-                /**
-                 * TODO: handle cases where the field has no corresponding key
-                 * Update fieldName and/or rawVal as necessary.
-                 */
-            }
+            JsonElement matchingElement = matching.getMatchingKey(fieldName, fieldType);
 
+            // Expecting an object
             if (refs.containsKey(i)) {
-                /**
-                 *  TODO: handle array case
-                 *  Suppose we have 2 tables A and B. If B.a is a foreign key to A.a and we
-                 *  want to insert {'a': 1, 'b': [1,2]} into A, ideally we insert 1 row into
-                 *  A, 2 rows into B and link the 2 rows in B to the row in A. However the
-                 *  current implementation ignores 'b' entirely.
-                 */
-                JsonObject val = rawVal.getAsJsonObject();
-                // recursively process references
-                int refTable = refs.get(i);
-                Tuple refTuple = jsonObjectToTuple(val, refTable);
-                TupleDesc refTd = refTuple.getTupleDesc();
-                // Note: this will fail if a table does not have a pkey column
-                int refColumn = refTd.fieldNameToIndex(catalog.getPrimaryKey(refTable));
-                tup.setField(i, refTuple.getField(refColumn));
+            	
+        		// Got primitive
+            	if(matchingElement.isJsonPrimitive()) {
+        			int referredTable = refs.get(i);
+        			String pKeyName = catalog.getPrimaryKey(referredTable);
+        			int pKeyIndex = catalog.getTupleDesc(referredTable).fieldNameToIndex(pKeyName);
+        			Type pKeyType = catalog.getTupleDesc(referredTable).getFieldType(pKeyIndex);
+        			if(pKeyType == Type.INT_TYPE) {
+        				if(!matchingElement.getAsJsonPrimitive().isNumber()) {
+                            throw new IllegalArgumentException("Unknown data type");
+        				}
+        				tup.setField(i, new IntField(matchingElement.getAsJsonPrimitive().getAsInt()));
+        			}
+        			else if(pKeyType == Type.STRING_TYPE) {
+        				tup.setField(i, new StringField(matchingElement.getAsJsonPrimitive().getAsString(), Type.STRING_LEN));
+        			}
+        		}
+            	
+            	// Got object
+            	else if(matchingElement.isJsonObject()) {
+	            		
+	                /**
+	                 *  TODO: handle array case
+	                 *  Suppose we have 2 tables A and B. If B.a is a foreign key to A.a and we
+	                 *  want to insert {'a': 1, 'b': [1,2]} into A, ideally we insert 1 row into
+	                 *  A, 2 rows into B and link the 2 rows in B to the row in A. However the
+	                 *  current implementation ignores 'b' entirely.
+	                 */
+	                JsonObject val = matchingElement.getAsJsonObject();
+	                // recursively process references
+	                int refTable = refs.get(i);
+	                Tuple refTuple = jsonObjectToTuple(val, refTable);
+	                TupleDesc refTd = refTuple.getTupleDesc();
+	                // Note: this will fail if a table does not have a pkey column
+	                int refColumn = refTd.fieldNameToIndex(catalog.getPrimaryKey(refTable));
+	                tup.setField(i, refTuple.getField(refColumn));
+	            }
+            	
+            	// Got something else
+            	else {
+                    throw new IllegalArgumentException("Unknown data type");
+            	}
             }
-            else {
-                JsonPrimitive val = rawVal.getAsJsonPrimitive();
-                if (fieldType == Type.INT_TYPE) {
-                    tup.setField(i, new IntField(val.getAsInt()));
+            // Expecting an integer
+            else if(fieldType == Type.INT_TYPE) {
+            	if(matchingElement.isJsonPrimitive()) {
+	                JsonPrimitive val = matchingElement.getAsJsonPrimitive();
+	                
+	                // Got number
+	                if (val.isNumber()) {
+	                    tup.setField(i, new IntField(val.getAsInt()));
+	                }
+	                
+	                // Got string
+	                else if (val.isString()) {
+	                    tup.setField(i, new IntField(Integer.parseInt(val.getAsString())));
+	                }
+            	}
+            	
+            	// Got anything else
+                else {
+                    throw new IllegalArgumentException("Unknown data type");
                 }
-                else if (fieldType == Type.STRING_TYPE) {
-                    tup.setField(i, new StringField(val.getAsString(), Type.STRING_LEN));
-                }
+            }
+            
+            // Expecting a string
+            else if(fieldType == Type.STRING_TYPE){
+            	if(matchingElement.isJsonPrimitive()) {
+	                JsonPrimitive val = matchingElement.getAsJsonPrimitive();
+	                
+	                // Got a string
+	                if (val.isString()) {
+	                    tup.setField(i, new StringField(val.getAsString(), Type.STRING_LEN));
+	                }
+	                
+	                // Got another primitive
+	                else {
+	                    tup.setField(i, new StringField(val.toString(), Type.STRING_LEN));
+	                }
+            	}
+            	
+            	// Got anything else
                 else {
                     throw new IllegalArgumentException("Unknown data type");
                 }
             }
         }
-
-        /**
-         * TODO: dump the extra fields into tuples
-         * You may find JsonObject.entrySet function helpful.
-         */
+        
+        // Expected nothing
+        for(Entry<String, JsonElement> unmatchedEntry : matching.unusedKeyValues()) {
+        	JsonElement element = unmatchedEntry.getValue();
+        	// Got any primitive
+        	if(element.isJsonPrimitive()) {
+        		logExtraField(element);
+        	}
+        	
+        	// Got an object
+        	if(element.isJsonObject()) {
+        		for(Iterator<Integer> tableIdIter = catalog.tableIdIterator(); tableIdIter.hasNext();) {
+        			int nextTable = tableIdIter.next();
+        			if(catalog.getTableName(nextTable) == "") {
+        				
+        			}
+        			Map<Integer, Integer> foreignKeys = catalog.getForeignKeys(nextTable);
+        			for(Entry<Integer, Integer> foreignKey : foreignKeys.entrySet()) {
+        				if(foreignKey.getValue()==table) {
+        					
+        				}
+        			}
+        			//foreignKeys.
+        		}
+        	}
+        }
 
         // append table info to the tuple
         TupleDesc infoTd = new TupleDesc(new Type[]{Type.INT_TYPE}, new String[]{null});
@@ -153,5 +233,9 @@ public class JsonScan implements OpIterator {
         tupBuffer.add(mergedTup);
         return mergedTup;
     }
+
+	private void logExtraField(JsonElement element) {
+		// TODO Auto-generated method stub
+	}
 
 }
